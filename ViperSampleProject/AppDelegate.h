d0b9e9dedef9560eb5a7aa45a8b31ef234cfdb58
Nikolay Chaban <nchaban.kh@gmail.com>
2016-12-22 16:15:52 +0200

    Initial Commit

---
+++ ViperSampleProject/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ViperSampleProject
+//
+//  Created by Nikolay Chaban on 12/22/16.
+//  Copyright © 2016 Nikolay Chaban. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ViperSampleProject/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ViperSampleProject
+//
+//  Created by Nikolay Chaban on 12/22/16.
+//  Copyright © 2016 Nikolay Chaban. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
